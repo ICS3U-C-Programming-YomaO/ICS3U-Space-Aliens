38b807fc88a1544b700a8830836c687b049a756c
Yoma Ozoh <ogeneyoma.ozoh@stu.ocsb.ca>
2026-01-12 18:59:23 +0000

    04

--- code.py
+++ code.py
@@ -4,12 +4,13 @@ import stage
 def game_scene():
     # this function is for the main game game_scene
 
-    # image banks for CircuitPython
+    # image banks for CircuitPython stores the images in bmp file
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
     image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
     # set the background to image 0 in the image bank
     # and the size (10x8 tiles of size 16x16)
     background = stage.Grid(image_bank_background, 10, 8)
+    # this is my ship sprite that can be moved
     ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
     # create a stage for the background to show up on
     # and set the frame rate to 60 fps
@@ -27,7 +28,7 @@ def game_scene():
         # update game logic
 
         #redraw sprite
-
+        # redraws game sprites
         game.render_sprites([ship])
         game.tick()
 
