5544fbabe5bf4d5a730de4bae5f89d5cfa6661ac
Yoma Ozoh <ogeneyoma.ozoh@stu.ocsb.ca>
2026-01-15 06:30:27 +0000

    08

--- code.py
+++ code.py
@@ -1,7 +1,50 @@
 import ugame
 import stage
-
+import time
+import random
 import constants
+
+def splash_scene():
+    # this function is for the main game splash_scene
+
+    # get sound ready
+    coin_sound = open("coin.wav", 'rb')
+    sound = ugame.audio
+    sound.stop()
+    sound.mute(False)
+    sound.play(coin_sound)
+
+    # image banks for CircuitPython stores the images in bmp file
+    # an image bank for CircuitPython
+    image_bank_mt_background = stage.Bank.from_bmp16("mt_game_studio.bmp")
+
+    # create a stage for the background to show up on
+    # and set the frame rate to 60fps
+    # sets the background to image 0 in the image bank
+    background = stage.Grid(image_bank_mt_background,constants.SCREEN_X,constants.SCREEN_Y)
+
+    # used this program to split the image into tile:
+    # https://ezgif.com/sprite-cutter/ezgif-5-818cdbcc3f66.png
+    background.tile(2, 2, 0)   # blank white
+    background.tile(3, 2, 1)
+
+    background.tile(6, 5, 0)
+    background.tile(7, 5, 0)   # blank white
+    game = stage.Stage(ugame.display, constants.FPS)
+    # set the layers of all sprites, items show up in order
+    game.layers = [background]
+    # render all sprites
+    # most likely you will only render the background  once per game scene
+    game.render_block()
+    
+    # repeat forever, game loop
+    while True:
+        # wait for 2 seconds and then goes to the menu scene, 
+        # it'll be a blank white scene
+        time.sleep(2.0)
+        menu_scene
+
+
 def menu_scene():
     # this function is for the main game game_scene
 
@@ -63,6 +106,13 @@ def game_scene():
     # set the background to image 0 in the image bank
     # and the size (10x8 tiles of size 16x16)
     background = stage.Grid(image_bank_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y)
+    # i have two for loops for x and y axis, I start at 0 and go to the grid size
+    for x_location in range(constants.SCREEN_GRID_X):
+        for y_location in range(constants.SCREEN_GRID_Y):
+            tile_picked = random.randint(1, 3)
+            # this will now create a different background
+            background.tile(x_location, y_location, tile_picked)
+
     # this is my ship sprite that can be moved, I subtract the sprite size x 2 from the screen so
     # the sprite starts at the bottom
     ship = stage.Sprite(image_bank_sprites, 5, 75, constants.SCREEN_Y - ( 2 * constants.SPRITE_SIZE))
@@ -114,7 +164,7 @@ def game_scene():
         if keys & ugame.K_UP:
             pass
         if keys & ugame.K_DOWN:
-            ship.move(ship.x, ship.y + 1)
+            ship.move(ship. x, ship.y + 1)
         # update game logic
         if a_button == constants.button_state["button_just_pressed"]:
             sound.play(pew_sound)
